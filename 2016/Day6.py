82c97a60216be94226c67d28c757e765b5dd8f4e
Anneroos <45787917+Anneroos@users.noreply.github.com>
2021-12-06 16:38:56 +0100

    Day 6

--- 2016/Day6.py
+++ 2016/Day6.py
@@ -1,11 +1,9 @@
 with open("input6.txt","r") as f:
     lines = f.read().splitlines()
-
 def mostCommonCharacter(list, index):
     letters = {}
     for word in list:
         letters[word[index]] = letters.get(word[index],0) + 1
-    print(letters)
     t=sorted(letters.keys(), key= lambda x: letters[x], reverse=True)
     return t
 message1 = ""
@@ -14,5 +12,5 @@ for i in range(len(lines[0])):
     sortedLetters = mostCommonCharacter(lines, i)
     message1 += sortedLetters[0]
     message2 += sortedLetters[-1]
-print(message1)
-print(message2)
+print(f"Part 1: The error-corrected version of the message is \"{message1}\".")
+print(f"Part 2: The original message is \"{message2}\".")
