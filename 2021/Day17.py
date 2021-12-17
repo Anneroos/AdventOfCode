56f13f65b10b17d731497b4ab0b745adc615431f
Anneroos <45787917+Anneroos@users.noreply.github.com>
2021-12-17 09:22:42 +0100

    Day 17 - Trick sot

---
+++ 2021/Day17.py
@@ -0,0 +1,22 @@
+x1, x2, y1, y2 = 139, 187, -148, -89
+def computeTrajectory(vx,vy):
+    x, y, vx, vy, maxy = 0, 0, vx, vy, 0
+    while vy >= 0 or y >= y1:
+        x += vx
+        y += vy
+        vx = max(vx-1,0)
+        vy -= 1
+        maxy = max(maxy, y)
+        if x1 <= x and x <= x2 and y1<= y and y <= y2:
+            return True, maxy
+    return False, 0
+highest, total = 0, 0
+for vx in range(x2+1):
+    for vy in range(y1,abs(y1)+1):
+        b, maxy = computeTrajectory(vx, vy)
+
+        if b:
+            total += 1
+            highest = max(maxy, highest)
+print(f"Part 1: The highest point that is possible in a feasible trajectory is {highest}.")
+print(f"Part 2: The total number of possible trajectories is {total}.")
