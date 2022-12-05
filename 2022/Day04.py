6fb30533026ca1136612b43e8affabf3ec54572c
Anneroos <45787917+Anneroos@users.noreply.github.com>
2022-12-05 08:30:30 +0100

    Day 4: Added alternative

--- 2022/Day04.py
+++ 2022/Day04.py
@@ -7,7 +7,7 @@ for k in inp:
     elf2 = k[1]
     if (elf1[0] <= elf2[0] and elf1[1] >= elf2[1]) or (elf1[0] >= elf2[0] and elf1[1] <= elf2[1]):
         completeleyOverlappingPairs += 1
-    if not (elf1[1] < elf2[0] or elf1[0] > elf2[1]):
+    if not (elf1[1] < elf2[0] or elf1[0] > elf2[1]): # Equivalent: if (elf1[1] >= elf2[0] and elf1[0] <= elf2[1]):
         overlappingPairs += 1
 print(f"Day 4:\n1) There are {completeleyOverlappingPairs} pairs that have completely overlapping ranges.")
 print(f"2) There are {overlappingPairs} pairs with overlapping ranges.")
