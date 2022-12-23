c0e97f07ea3d0d6ff44437b81c42b04fad7b6c94
Anneroos <45787917+Anneroos@users.noreply.github.com>
2022-12-23 09:52:32 +0100

    Day 19: Finally made a robot factory

--- 2022/Day19.py
+++ 2022/Day19.py
@@ -1,22 +1,23 @@
+# Quite a slow program, but it does the job.
+# I have gotten some ideas from Reddit:
+# - check decisions (which robots to make next) in stead of checking every minute what is possible
+# - prune the paths that are hopeless, by checking the best geode score possible if you would make a geode robot every minute from now on
+# - if you already have enough ore robots to make every robot you want for the rest of the time, don't make more ore robots
+# and from Jacob:
+# - make a class, and let it make copies of it self
+
 import re
 import math
 import copy
 
 with open("input19.txt") as f:
     lines = f.read().split("\n")
+
+# Read in the input and store the blueprints in a list of dictionaries
 Blueprints = []
 for line in lines:
     linestring = "Blueprint (d+)\: Each ore robot costs (d+) ore. Each clay robot costs 3 ore. Each obsidian robot costs 3 ore and 8 clay. Each geode robot costs 3 ore and 12 obsidian."
-
     matchObj = re.match(r'^Blueprint (\d+): Each ore robot costs (\d+) ore. Each clay robot costs (\d+) ore. Each obsidian robot costs (\d+) ore and (\d+) clay. Each geode robot costs (\d+) ore and (\d+) obsidian.$', line)
-    
-    # blueprintnr = int(matchObj.group(1))
-
-    # orerobotcost = (int(matchObj.group(2)), 0, 0)
-    # clayrobotcost = (int(matchObj.group(3)), 0, 0)
-    # obsidianrobotcost = (int(matchObj.group(4)),  int(matchObj.group(5)), 0)
-    # geoderobotcost = (int(matchObj.group(6)), 0, int(matchObj.group(7)))
-    # blueprinttuple = (orerobotcost, clayrobotcost, obsidianrobotcost, geoderobotcost, blueprintnr)
     blueprint = {"name": int(matchObj.group(1)),
                  "ore": {"ore":int(matchObj.group(2)), "clay": 0, "obsidian":0},
                  "clay": {"ore": int(matchObj.group(3)), "clay": 0, "obsidian": 0},
@@ -25,7 +26,7 @@ for line in lines:
                  }
     Blueprints.append(blueprint)
 
-
+# In this class robotState, I keep the state of one decision-path. I initialize it, and make copies to compute different paths
 class robotState():
     def __init__(self, blueprint, minutes):
         self.bp = blueprint
@@ -35,15 +36,13 @@ class robotState():
         self.maxNeeded = {}
         for res in ["ore", "clay", "obsidian" ]:
             self.maxNeeded[res] = max([self.bp[rob][res] for rob in self.robots])
-        print(self.maxNeeded)
-        self.history = []
-        # self.listOfRobots = listOfRobots
-    def computeNextSteps(self):
+        self.history = [] # To check what the decisions were for the winning strategy
+    def computeNextSteps(self): # Consider the different robot types, and decide which ones to build next
+        # If the bestpossible score (by creating a geode robot every minute from now on) is less than the overall bestGeodeScore, stop this path
         bestPossibleScore = self.resources["geode"] + self.minutes*self.robots["geode"] + self.minutes*(self.minutes-1)/2
         if bestPossibleScore < bestGeodeScore:
             self.finishTime()
             geodeScore = self.resources["geode"]
-            # finishedRobots.append(self)
             return geodeScore
         else:
             copiesMade = 0
@@ -53,22 +52,20 @@ class robotState():
                     if T < self.minutes:
                         self.makeCopy(T+1, robottype)
                         copiesMade += 1
-            if copiesMade == 0:
+            if copiesMade == 0: # If there is not enough time left to build one of the robot, then finish the time and stop this path
                 self.finishTime()
                 geodeScore = self.resources["geode"]
-                # finishedRobots.append(self)
                 return geodeScore
             else:
                 return -1
 
-    def finishTime(self):
+    def finishTime(self): # Let time run out, and add the resources that are still gathered by the current robots
         for res in self.robots:
             self.resources[res] += self.minutes*self.robots[res]
         self.minutes = 0
-    def computeTimeToWaitForNextRobot(self, robottype):
+    def computeTimeToWaitForNextRobot(self, robottype): # Compute how much time is needed to gain enough matierials to build a certain robot
         needs = self.bp[robottype]
         minTime = -1
-
         for res in needs:
             if needs[res] >0:
                 if self.robots[res] == 0:
@@ -77,68 +74,55 @@ class robotState():
                     T = math.ceil((needs[res] - self.resources[res] )/ self.robots[res])
                     if T >= minTime:
                         minTime = T
-                # print(f"robottype {robottype}, resource {res},mintime{minTime}, T {T}. self.robots[res] {self.robots[res]}, needs[res] {needs[res]},self.resources[res] {self.resources[res]}")
-        # print(f"Final mintime {minTime}")
-        minTime = self.minutes if minTime == -1 else minTime #max(1, minTime)
+        minTime = self.minutes if minTime == -1 else minTime
         return minTime
 
     def makeCopy(self, T, robottype):
         newRobotState = copy.deepcopy(self)
-        # Get new resources from the robots
+        # Get new resources from the robots for the past T minutes
         for res in newRobotState.robots:
             newRobotState.resources[res] += T*newRobotState.robots[res]
         # Add a new robot
         newRobotState.robots[robottype] += 1
         # Pay for the new robot
-        # print("Paying! old resources, after adding from robots", newRobotState.resources)
         for res in self.bp[robottype]:
             newRobotState.resources[res] -= newRobotState.bp[robottype][res]
-        # print( "after paying", newRobotState.resources)
         newRobotState.minutes -= T
         newRobotState.history.append([newRobotState.minutes, robottype, T, copy.deepcopy(newRobotState.robots), copy.deepcopy(newRobotState.resources)])
-        # print("minutes left for new robot", newRobotState.minutes)
         robotList.append(newRobotState)
-#
-# # Part 1
-# qualityLevels = []
-# for bp in Blueprints:
-#     print("Blueprint:", bp)
-#     r = robotState(bp,24)
-#     robotList = [r]
-#     bestGeodeScore = 0
-#     bestPath = None
-#     finishedRobots = []
-#     while len(robotList) > 0:
-#         rob = robotList.pop()
-#         geodeScore = rob.computeNextSteps()
-#         if geodeScore >= bestGeodeScore:
-#             bestGeodeScore = geodeScore
-#             bestPath = rob.history
-#     print(f"QUality level = {bp['name'] * bestGeodeScore}, Best Geode score for this blueprint {bp['name']} is {bestGeodeScore}")
-#     qualityLevels.append(bp["name"] * bestGeodeScore)
-# print(qualityLevels)
-# print(sum(qualityLevels))
 
-
-# Part 2
-geodeScores = []
-for bp in Blueprints[:3]:
-    print("Blueprint:", bp)
-    r = robotState(bp,32)
+# Part 1
+qualityLevels = []
+for bp in Blueprints:
+    r = robotState(bp, 24)
     robotList = [r]
     bestGeodeScore = 0
-    bestPath = None
+    # bestPath = None
     finishedRobots = []
     while len(robotList) > 0:
         rob = robotList.pop()
         geodeScore = rob.computeNextSteps()
         if geodeScore >= bestGeodeScore:
-            print("new highscore!", geodeScore)
             bestGeodeScore = geodeScore
-            bestPath = rob.history
-    print(f"Quality level = {bp['name'] * bestGeodeScore}, Best Geode score for this blueprint {bp['name']} is {bestGeodeScore}")
+            # bestPath = rob.history
+    qualityLevels.append(bp["name"] * bestGeodeScore)
+
+print(f"Day 19:\n1) The sum of the quality levels of the blueprints is \033[1m{sum(qualityLevels)}\033[0m.")
+
+# Part 2
+geodeScores = []
+for bp in Blueprints[:3]:
+    r = robotState(bp,32)
+    robotList = [r]
+    bestGeodeScore = 0
+    # bestPath = None
+    finishedRobots = []
+    while len(robotList) > 0:
+        rob = robotList.pop()
+        geodeScore = rob.computeNextSteps()
+        if geodeScore >= bestGeodeScore:
+            bestGeodeScore = geodeScore
+            # bestPath = rob.history
     geodeScores.append(bp["name"] * bestGeodeScore)
 
-print(geodeScores)
-print(math.prod(geodeScores))
-# 12240 too high!
+print(f"2) The maximum number of geodes possible for the first 3 blueprints after 32 minutes are {geodeScores}. The product of these scores is \033[1m{math.prod(geodeScores)}\033[0m.")
